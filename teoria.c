8bfb5459870586b797519906a56d5880767bc207
Juanmaacosta1 <acostajuanma854@gmail.com>
2024-05-11 01:42:04 -0300

    Add files via upload

---
+++ teoria.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int num1 = 7;
+int num2= 7;
+
+if (num1 < num2) {
+    printf("num1 es mas chico que un num1");
+} else if (num1== num2) {
+    printf("son ingales ");
+} else {
+    printf("num2 es mayor ");
+}
+return 0;
+}
