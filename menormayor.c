29bd73daffcd4cb33926b4146a5b9dfe99efb46f
Juanmaacosta1 <acostajuanma854@gmail.com>
2024-05-11 01:39:14 -0300

    Add files via upload

---
+++ menormayor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+int num;
+
+printf("ingrese su num :");
+scanf("%d",&num);
+    if (num < 10)
+    {
+        printf("numero menor a 10");
+    } else
+    {
+        printf("numero mayor a 10");
+    }
+        return 0;
+}
