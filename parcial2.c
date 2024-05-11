3f31d65971e6f07472dc1bee0632532462ea42fd
Juanmaacosta1 <acostajuanma854@gmail.com>
2024-05-11 01:33:56 -0300

    Add files via upload

---
+++ parcial2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    
+    // Imprimir elementos del arreglo
+    printf("Elementos del arreglo: ");
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    // Tu otro bucle iría aquí
+    /*
+    for (int i = 0; i < count; i++) {
+        // código
+    }
+    */
+   
+    return 0;
+}
